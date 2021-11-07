ddf3571d067fc74f31e457bb0b1fa02bc249584d
sforesi <sebastiano.foresi@gmail.com>
2021-11-07 09:42:12 -0500

    add price to intrument model

--- models/instrument.js
+++ models/instrument.js
@@ -5,7 +5,8 @@ const Schema = mongoose.Schema
 const instrumentSchema = new Schema({
   name: String,
   reserved: Boolean,
-  owner: {type: Schema.Types.ObjectId, ref: "Profile"}
+  owner: {type: Schema.Types.ObjectId, ref: "Profile"},
+  price: {type: Number, min: 0, max: 1000}
 })
 
 const Instrument = mongoose.model('Instrument', instrumentSchema)
