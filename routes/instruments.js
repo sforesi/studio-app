5447d731e6111ba74642b1969525d44690c75a1f
sforesi <sebastiano.foresi@gmail.com>
2021-11-07 11:11:30 -0500

    edit/update instrument function working

--- routes/instruments.js
+++ routes/instruments.js
@@ -16,8 +16,6 @@
 
 
 
-// // localhost:3000/instruments/:id - PUT
-// router.put("/:id", isLoggedIn, instrumentsCtrl.update)
 
 // // localhost:3000/instruments/:id - DELETE
 // router.delete("/:id", isLoggedIn, instrumentsCtrl.delete)
@@ -26,29 +24,32 @@
 // router.get("/new", instrumentsCtrl.new)
 
 // export {
-//   router
-// }
-
-import { Router } from 'express'
-import * as instrumentsCtrl from '../controllers/instruments.js'
-import { isLoggedIn } from '../middleware/middleware.js'
-
-const router = Router()
-
-// localhost:3000/instruments/new - GET
-router.get("/new", instrumentsCtrl.new)
-// localhost:3000
-router.get("/", instrumentsCtrl.index)
-// localhost:3000/instruments/:id
-router.get("/:id", instrumentsCtrl.show)
-// localhost:3000/instruments/:id/edit
-router.get("/:id/edit", instrumentsCtrl.edit)
-
-// localhost:3000/instruments
-router.post("/", isLoggedIn, instrumentsCtrl.create)
-
-// localhost:3000/instrument/:id/switch-reserved - PATCH
-router.patch("/:id/switch-reserved", isLoggedIn, instrumentsCtrl.switchReserved)
+  //   router
+  // }
+  
+  import { Router } from 'express'
+  import * as instrumentsCtrl from '../controllers/instruments.js'
+  import { isLoggedIn } from '../middleware/middleware.js'
+  
+  const router = Router()
+  
+  // localhost:3000/instruments/new - GET
+  router.get("/new", instrumentsCtrl.new)
+  // localhost:3000
+  router.get("/", instrumentsCtrl.index)
+  // localhost:3000/instruments/:id
+  router.get("/:id", instrumentsCtrl.show)
+  // localhost:3000/instruments/:id/edit
+  router.get("/:id/edit", instrumentsCtrl.edit)
+  
+  // localhost:3000/instruments
+  router.post("/", isLoggedIn, instrumentsCtrl.create)
+  
+  // localhost:3000/instrument/:id/switch-reserved - PATCH
+  router.patch("/:id/switch-reserved", isLoggedIn, instrumentsCtrl.switchReserved)
+  
+  // localhost:3000/instruments/:id - PUT
+  router.put("/:id", isLoggedIn, instrumentsCtrl.update)
 
 export {
   router
